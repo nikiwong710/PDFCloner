7c959db9e909338d056259c94e517c755f10ad36
SnagRNiki <NIki@Niki.com>
2026-05-29 11:26:46 +0800

    Trigger Cloudflare deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Trigger Cloudflare deployment
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
